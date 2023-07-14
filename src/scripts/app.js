79dac30d391eaf9916d41be9eaddb21454314075
joshrandall8478 <joshrandall8478@gmail.com>
2023-07-14 19:08:35 -0400

    more translation

---
+++ src/scripts/app.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        return document.querySelector('.navbar').classList.add('hide')
+    }
+    return document.querySelector('.navbar').classList.remove('hide')
+})
